9ff04ddcb40b5a33e02dbc9c70faba5e38c85bd7
White Lily <lili007white@gmail.com>
2022-09-13 16:04:17 +0300

    внесла изменения по замечаниям

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -2,42 +2,47 @@ import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 
 public class LionTest {
     private final String defaultSex = "Самец";
 
-  Feline feline;
-
     @Test
     public void doesHaveManeMaleReturnTrue() throws Exception {
-        String sex = "Самец";
-        Lion lion = new Lion(sex, feline);
-        boolean expectedValue = true;
-        Assert.assertEquals(expectedValue, lion.doesHaveMane());
+        Feline feline = Mockito.mock(Feline.class);
+
+        Assert.assertTrue("У льва должна быть грива",
+                new Lion("Самец", feline).doesHaveMane());
     }
 
     @Test
     public void doesHaveManeFemaleReturnFalse() throws Exception {
-        String sex = "Самка";
-        Lion lion = new Lion(sex, feline);
-        boolean expectedValue = false;
-        boolean actualValue = lion.doesHaveMane();
-        Assert.assertEquals(expectedValue, lion.doesHaveMane());
+        Feline feline = Mockito.mock(Feline.class);
+        Assert.assertFalse(
+                "У львицы не должна быть грива",
+                new Lion("Самка", feline).doesHaveMane());
     }
 
     @Test
     public void getKittensReturnCorrectInt() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+
         Lion lion = new Lion(defaultSex, feline);
         Assert.assertEquals(1, lion.getKittens());
     }
 
     @Test
     public void lionGetFood() throws Exception {
-        Lion lion = new Lion(defaultSex, feline);
+        Feline feline = Mockito.mock(Feline.class);
         List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.getFood(anyString())).thenReturn(expectedFood);
+        Lion lion = new Lion(defaultSex, feline);
         Assert.assertEquals(expectedFood, lion.getFood());
     }
 }
